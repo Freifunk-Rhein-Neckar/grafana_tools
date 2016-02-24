b5beaabacdd4b1fa1c408b794621b01de89066a5
Leah Oswald <mail@leahoswald.de>
2016-02-24 23:36:34 +0100

    add module to get the active dhcp leases

---
+++ dhcp_leases.py
@@ -0,0 +1,7 @@
+from isc_dhcp_leases.iscdhcpleases import Lease, IscDhcpLeases
+import socket
+import time
+
+leases = IscDhcpLeases('/var/lib/dhcp/dhcpd.leases')
+active_leases = leases.get_current()
+print('ffrn.' + socket.gethostname() + '.dhcp.active_leases ' + str(len(active_leases)) + ' ' + str(int(time.time())))
